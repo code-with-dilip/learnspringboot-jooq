52f8f91d3bc9fc169c500576f79cdc6f422e7966
Dilip Sundarraj <dilip.sundarraj@target.com>
2019-06-13 09:42:02 -0500

    refactored the code

--- src/main/kotlin/com/learnjooq/LearnspringbootJooqApplication.kt
+++ src/main/kotlin/com/learnjooq/LearnspringbootJooqApplication.kt
@@ -1,4 +1,4 @@
-package com.learnjooq.learnspringbootjooq
+package com.learnjooq
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/com/learnjooq/LearnspringbootJooqApplicationTests.kt
+++ src/test/kotlin/com/learnjooq/LearnspringbootJooqApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.learnjooq.learnspringbootjooq
+package com.learnjooq
 
 import org.junit.Test
 import org.junit.runner.RunWith
