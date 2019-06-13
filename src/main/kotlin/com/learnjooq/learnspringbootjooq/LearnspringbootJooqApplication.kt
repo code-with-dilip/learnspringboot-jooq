a09d5c1d6e955f80d3a33b0c0228d101e48aab38
Dilip Sundarraj <dilip.sundarraj@target.com>
2019-06-13 09:37:43 -0500

    added the intial repo

---
+++ src/main/kotlin/com/learnjooq/learnspringbootjooq/LearnspringbootJooqApplication.kt
@@ -0,0 +1,11 @@
+package com.learnjooq.learnspringbootjooq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnspringbootJooqApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearnspringbootJooqApplication>(*args)
+}
---
+++ src/test/kotlin/com/learnjooq/learnspringbootjooq/LearnspringbootJooqApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.learnjooq.learnspringbootjooq
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class LearnspringbootJooqApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
